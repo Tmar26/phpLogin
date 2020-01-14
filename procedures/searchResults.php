51a5741032934e9f7d75f3e51ddc75d845985ff8
Tamara McDiarmid <tmar26@me.com>
2020-01-14 01:48:50 -0500

    fixed nav bar issue on searchresults

--- procedures/searchResults.php
+++ procedures/searchResults.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../inc/bootstrap.php';
 require_once __DIR__ . '/../templates/header.php';
-require_once __DIR__ . '/../templates/nav.php';
+//require_once __DIR__ . '/../templates/nav.php';
 
 foreach (searchResults($_POST['searchTitle']) as $dvd) {
   include __DIR__ . '/../templates/search.php';
