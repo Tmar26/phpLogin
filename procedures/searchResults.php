20eec3dbeddd58956d82e148d3f01fcac5860eca
Tamara McDiarmid <tmar26@me.com>
2020-01-14 01:40:05 -0500

    added go back to list button

--- procedures/searchResults.php
+++ procedures/searchResults.php
@@ -7,5 +7,8 @@ require_once __DIR__ . '/../templates/nav.php';
 foreach (searchResults($_POST['searchTitle']) as $dvd) {
   include __DIR__ . '/../templates/search.php';
 }
-
-require_once __DIR__ . '/../templates/footer.php';
+?>
+    <form method="post" action="../dvds.php">
+      <input type="submit" value="GO BACK TO DVD LIST" name="returnToList">
+    </form>
+<?php require_once __DIR__ . '/../templates/footer.php';
