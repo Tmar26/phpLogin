44ac52d6082a5f052fa1ed6562a8bd1180f6a31f
Tamara McDiarmid <tmar26@me.com>
2020-01-23 15:50:57 -0500

    delete dvd and image fct added

--- procedures/deleteDvd.php
+++ procedures/deleteDvd.php
@@ -1,16 +1,20 @@
 <?php
 require_once __DIR__ . '/../inc/bootstrap.php';
 requireAuth();
-
+$path = '../';
 $dvd = getDvd(request()->get('dvdId'));
+$filename = $dvd['image'];
 if (!isAdmin() && !isOwner($dvd['owner_id'])) {
   $session->getFlashBag()->add('error', 'not authorized');
   redirect('../dvds.php');
 }
-
-if (deleteDvd($dvd['id'])) {
+//removes the dvd entry from the db and removes the image from the uploads folder
+if (deleteDvd($dvd['id']) && unlink($path . $filename)) {
   $session->getFlashBag()->add('success', 'DVD Deleted');
 } else {
-  $session->getFlashBag()->add('error', 'Unable to Delete DVD');
+  $session->getFlashBag()->add('error', 'Unable to delete DVD');
 }
 redirect('../dvds.php');
+
+
+
